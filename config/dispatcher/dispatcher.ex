937063398f986353f475c884ec38d945aa0a7fd3
Nóra Szepes <nora.szepes@tenforce.com>
2017-03-22 15:08:05 +0100

    added docker-compose and dispatcher configuration for the backend

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -34,6 +34,10 @@ defmodule Dispatcher do
     Proxy.forward conn, path, "http://resource/container-groups/"
   end
 
+  match "/stack-builder-backend/*path" do
+    Proxy.forward conn, path, "http://backend/"
+  end
+
   match _ do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
   end
